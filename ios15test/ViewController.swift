2ff59be4291418ac430f85e87ab3ecdb50abb0ea
Alexander von Below <Alex@vonBelow.Com>
2021-06-30 21:58:34 +0200

    Initial Commit

---
+++ ios15test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios15test
+//
+//  Created by Alexander v. Below on 30.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
